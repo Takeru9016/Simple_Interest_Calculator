1ba4e384db4b9d5ff4ffc4c6cda82281de8d4044
Takeru9016 <youthfox046@gmail.com>
2022-06-01 19:44:36 +0530

    Updated file

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (principal < 1) {
+    alert("Enter a positive number");
+    return false;
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  var res =
+    "If you deposit, <span class = 'highlight'>" +
+    principal +
+    "</span> ,<br> at an interest rate of <span class = 'highlight'>" +
+    rate +
+    "</span>. <br> You will receive an amount of <span class = 'highlight'>" +
+    interest +
+    "</span>, <br> in the year <span class = 'highlight'>" +
+    year;
+  document.getElementById("result").innerHTML = res;
+  console.log("inner html executed");
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+}
